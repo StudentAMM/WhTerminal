8e64ffd8ebfe6942ca28931729664e19571b3813
Коротких А.А <evolenteam@mail.ru>
2019-07-30 22:52:15 +0300

    test

--- WhTerminal/Controllers/ValuesController.cs
+++ WhTerminal/Controllers/ValuesController.cs
@@ -4,15 +4,24 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using WhTerminal.Data.DataAccess;
+using WhTerminal.Data.Models;
 
 namespace WhTerminal.Controllers
 {
+    [Route("api/[controller]")]
     public class ValuesController : ApiController
     {
+        private UnitOfWork unitOfWork = new UnitOfWork();
+
         // GET api/values
-        public IEnumerable<string> Get()
+        public IHttpActionResult Get()
         {
-            return new string[] { "value1", "value2" };
+            // SHOWSTATUS 
+            List<int> wh = new List<int>();
+            foreach (Warehouse i in unitOfWork.WarehouseRepository.GetAll())
+                wh.Add(i.Id);
+            return Ok(wh);
         }
 
         // GET api/values/5
